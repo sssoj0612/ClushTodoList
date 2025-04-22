239e118c2e1c38cb499d41d9cd2ec972b7934c02
sj.kim <sj.kim@dc-on.co.kr>
2025-04-22 14:50:21 +0900

    백  프론트 연결결

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,50 +1,61 @@
 import "./App.css";
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
-import List from "./components/List";
-
-const mockData = [
-  {
-    id: 0,
-    isDone: false,
-    content: "React 공부하기",
-    date: new Date().getTime(),
-  },
-  {
-    id: 1,
-    isDone: false,
-    content: "또 공부하기",
-    date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    isDone: false,
-    content: "계속 공부하기",
-    date: new Date().getTime(),
-  },
-];
+//import List from "./components/List";
+import TodoItem from "./components/TodoItem";
+import axios from "axios";
 
 function App() {
-  const [todos, setTodos] = useState(mockData);
-  const idRef = useRef(3);
+  const [todos, setTodos] = useState([]);
+  
+  // 1. 처음 마운트될 때 전체 리스트트 불러오기
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
-  const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
+  const fetchTodos = async () => {
+    const res = await axios.get("http://localhost:11000/api/todo/list");
+    setTodos(res.data);
+  };
 
-    setTodos([newTodo, ...todos]);
+  // 2. 등록
+  const onCreate = async (contents) => {
+    await axios.post("http://localhost:11000/api/todo/insert", {
+      contents,
+    });
+    fetchTodos(); // 갱신!!!
+  };
+
+  // 3. 삭제
+  const onDelete = async (todoSeq) => {
+    await axios.delete(`http://localhost:11000/api/todo/delete/${todoSeq}`);
+    fetchTodos();
+  };
+
+  // 4. 상태 변경
+  const onToggle = async (todo) => {
+    await axios.put("http://localhost:11000/api/todo/update", {
+      ...todo,
+      status: !todo.status,
+    });
+    fetchTodos();
   };
 
   return (
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} />
+      <div className="TodoList">
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.todoSeq}
+            todo={todo}
+            onDelete={onDelete}
+            onToggle={onToggle}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- front/src/components/TodoItem.jsx
+++ front/src/components/TodoItem.jsx
@@ -1,11 +1,15 @@
 import "./TodoItem.css";
 
-const TodoItem = () => {
+const TodoItem = ({ todo, onDelete, onToggle }) => {
+
+    const { todoSeq, contents, regDt, status } = todo;
+
     return (
         <div className="TodoItem">
-            <input type="checkbox"/>
-            <div className="contents">Todo...</div>
-            <button>삭제</button>
+            <input type="checkbox" checked={status} onChange={() => onToggle(todo)}/>
+            <div className="contents">{contents}</div>
+            <div className="date">{regDt}</div>
+            <button onClick={() => onDelete(todoSeq)}>삭제</button>
         </div>
     );
 };
